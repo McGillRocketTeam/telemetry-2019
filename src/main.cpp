22fcdd2775ad69b7719aa557cc4b8c1127a74c74
Kira <kira.e.noel@gmail.com>
2019-02-16 19:52:36 -0500

    fixed some bugs; compiles

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,7 @@
 #include <SD.h>
 #include <utility/imumaths.h>
 
-#define numBiasMeas 10  // number of times to measure the bias
-
-Adafruit_BMP280_Unified bmp;
+Adafruit_BMP280 bmp;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 // Configuration vars
@@ -21,9 +19,16 @@ String header = "millis, Altitude, Pressure, Temperature, Accel_x, Accel_y, Acce
 const int card_chipSelect = BUILTIN_SDCARD;
 
 // Functional vars
-double bias = 0.0;
+int file_num = 0;  // file number iterator
+String datastring;
+float ground_alt;
+unsigned long previous_file_size = 0;
 float temperature, pressure, alt, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z, 
       accel_x, accel_y, accel_z, time_passed, t_previous, alt_previous, vel, accel_magnitude;
+
+static int create_file();
+void read_data();
+static void store_data();
 
 void setup() {
   Serial.begin(9600);
@@ -35,9 +40,12 @@ void setup() {
 
   SD.begin(card_chipSelect);
   create_file();
-  set_bias();
-
+  ground_alt = bmp.readAltitude();
+  
   delay(1000);
+
+  alt_previous = ground_alt;
+  t_previous = millis();
 }
 
 void loop() {
@@ -45,15 +53,9 @@ void loop() {
   store_data();
 }
 
-void set_bias() {
-  for (int i = 0; i < numBiasMeas; i++) {
-    bias += getPressure();
-  }
-  bias /= numBiasMeas;
-}
-
 static int create_file() {
   // TODO
+  return 0;
 }
 
 void read_data()
@@ -68,14 +70,14 @@ void read_data()
   gyro_y = gyro_euler.y();
   gyro_z = gyro_euler.z();
 
-  imu::Vector<3> mag_euler = bno.getVector(Adafruit_BNO055::MAGNETOMETER);
+  imu::Vector<3> mag_euler = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
   mag_x = mag_euler.x();
   mag_y = mag_euler.y();
   mag_z = mag_euler.z();
 
-  bmp.readTemperature(&temperature);
-  bmp.readPressure(&pressure);
-  alt = bmp.pressureToAltitude(bias, pressure);
+  temperature = bmp.readTemperature();
+  pressure = bmp.readPressure();
+  alt = bmp.readAltitude() - ground_alt;
 
   time_passed = millis();
 
@@ -87,8 +89,58 @@ void read_data()
    
   //Read from GPS
   // TODO
+
+  // Edit to add GPS stuff
+  datastring = "";
+  datastring += alt;
+  datastring += ",";
+  datastring += String(time_passed,0);
+  datastring += ",";
+  datastring += temperature;
+  datastring += ",";
+  datastring += vel;
+  datastring += ",";
+  datastring += accel_magnitude;
+  datastring += ",";
+  datastring += "E";
+
+  // SEND TO XBEE
+  // TODO
 }
 
-static void store_data(){
-  // TODO
+static void store_data() {
+  datastring += String(accel_x,6);
+  datastring += ",";
+  datastring += String(accel_y,6);
+  datastring += ",";
+  datastring += String(accel_z,6);
+  datastring += ",";
+  datastring += String(mag_x,6);
+  datastring += ",";
+  datastring += String(mag_y,6);
+  datastring += ",";
+  datastring += String(mag_z,6);
+  datastring += ",";
+  datastring += String(gyro_x,6);
+  datastring += ",";
+  datastring += String(gyro_y,6);
+  datastring += ",";
+  datastring += String(gyro_z,6);
+  Serial.println(datastring); 
+  
+  String file_name = file_prefix + file_num + file_type;
+  char data_file_char_array[file_name.length() + 1];
+  file_name.toCharArray(data_file_char_array, file_name.length() + 1);
+
+  if (SD.exists(data_file_char_array)){
+   File dataFile = SD.open(data_file_char_array, FILE_WRITE);
+
+    if (dataFile) {
+      dataFile.println(datastring);  // write to SD card
+      if (dataFile.size() > previous_file_size) {
+        previous_file_size = dataFile.size();
+      }
+      dataFile.close();
+    }
+  }
 }
